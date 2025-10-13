aadb05df154e80abce12ee75367194b4a76a9908
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-10-13 12:48:12 -0600

    Se crean la operaciones crud para presupuestos

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,4 +2,7 @@ import { Sequelize } from "sequelize-typescript"
 import dotenv from "dotenv"
 dotenv.config()
 
-export const db = new Sequelize(process.env.DATABASE_URL, {})
+export const db = new Sequelize(process.env.DATABASE_URL, {
+    models:[__dirname + '/../models/**/*'],
+    logging: false
+})
